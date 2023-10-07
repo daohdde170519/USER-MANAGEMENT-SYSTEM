91af14831716aae298496b95ac6b7f38e0cf0501
DAO <daohdde170519@fpt.edu.vn>
2023-10-07 14:02:00 +0700

    First Commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+
+import controller.User;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author DAO
+ */
+public class Main {
+    public static void main(String[] args) {
+            User us =new User();
+            us.run1();
+    }
+}
